c7bc4252447d44ba09764714d4cc5ce6e879dc5d
Kedar Mhaswade <kedar@jymob.com>
2012-05-22 20:19:18 -0700

    YARA (Yet another rails app)

---
+++ clientapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clientapp::Application.config.secret_token = '3d62a4d6e07acd798069fc16b8ba257b9daeda8015e64f7d6606a690d1ae78227ddd4173bd095d4d594d7f2d866e273b0c27d7bd99fa1e59391e92e96c415b94'
